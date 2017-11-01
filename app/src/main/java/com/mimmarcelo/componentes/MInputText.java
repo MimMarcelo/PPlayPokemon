e285ec726bade2943862c71a9d2a36a02139ab4e
Marcelo Júnior <rokermarcelo@gmail.com>
2017-11-01 15:28:07 -0300

    Versão do campo de entrada de texto com label

---
+++ app/src/main/java/com/mimmarcelo/componentes/MInputText.java
@@ -0,0 +1,76 @@
+package com.mimmarcelo.componentes;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.mimmarcelo.pplaypokmon.R;
+
+/**
+ * Created by Marcelo Júnior on 31/10/2017.
+ */
+
+public class MInputText extends LinearLayout {
+    private TextView txtInputText;
+    private EditText edtInputText;
+
+    private String _placeholder;
+//    private OnClickListener edtClique = new OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            EditText e = (EditText)view;
+//            e.setHint("");
+//        }
+//    };
+    private OnFocusChangeListener edtChangeListener = new OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean b) {
+            EditText e = (EditText)view;
+            if(b) {
+                e.setHint("");
+            }
+            else{
+                if(e.getText().toString().isEmpty()){
+                    e.setHint(_placeholder);
+                }
+            }
+        }
+    };
+
+    public MInputText(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public MInputText(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public MInputText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        View v = inflate(context, R.layout.m_inputtext, null);
+        txtInputText = (TextView)v.findViewById(R.id.txtInputText);
+        edtInputText = (EditText)v.findViewById(R.id.edtInputText);
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.MInputText, defStyleAttr, 0);
+
+        txtInputText.setText(a.getString(R.styleable.MInputText_label));
+        edtInputText.setText(a.getString(R.styleable.MInputText_valor));
+        _placeholder = a.getString(R.styleable.MInputText_placeholder);
+        edtInputText.setHint(_placeholder);
+
+        edtInputText.setOnFocusChangeListener(edtChangeListener);
+
+        this.addView(v);
+    }
+}
